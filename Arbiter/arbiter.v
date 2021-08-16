62cf0dab702662542df49c3742e18bae7f646be1
Hasindu <khpiyumantha@gmail.com>
2021-08-17 02:03:07 +0530

    Fixed bugs in arbiter: Establishing a new connection when already connected to a slave.

--- Arbiter/arbiter.v
+++ Arbiter/arbiter.v
@@ -14,8 +14,8 @@ module arbiter(input clk, reset,
     parameter [2:0] idle = 3'd0;
     parameter [2:0] msb1 = 3'd1;
     parameter [2:0] msb2 = 3'd2;
-    parameter [2:0] pause = 3'd3;
-    parameter [2:0] connect = 3'd4;
+    parameter [2:0] connect = 3'd3;
+    parameter [2:0] busy = 3'd4;
 
     always @(posedge clk) begin
         if (reset)  state <= idle;
@@ -33,12 +33,22 @@ module arbiter(input clk, reset,
 
                 msb2: begin
                     address_buf <= {address_buf[0], m1_address};
-                    state <= pause;
+                    state <= connect;
+                end
+                
+                connect: begin
+                    if (connect1 || connect2 || connect3)
+                        state <= busy;
+                    else
+                        state <= idle;
                 end
 
-                pause:      state <= connect;
-                
-                connect:    state <= idle;
+                busy: begin
+                    if (m1_address_valid)
+                        state <= msb1;
+                    else
+                        state <= busy;
+                end
 
                 default:    state <= idle;
             endcase
@@ -82,15 +92,15 @@ module arbiter(input clk, reset,
 
     assign s1_address = (connect1) ? m1_address : 0;
     assign s1_data = (connect1) ? m1_data : 0;
-    assign s1_valid = (connect1) ? m1_valid : 0;
+    assign s1_valid = (connect1 && (state != msb1 && state != msb2)) ? m1_valid : 0;
 
     assign s2_address = (connect2) ? m1_address : 0;
     assign s2_data = (connect2) ? m1_data : 0;
-    assign s2_valid = (connect2) ? m1_valid : 0;
+    assign s2_valid = (connect2 && (state != msb1 && state != msb2)) ? m1_valid : 0;
 
     assign s3_address = (connect3) ? m1_address : 0;
     assign s3_data = (connect3) ? m1_data : 0;
-    assign s3_valid = (connect3) ? m1_valid : 0;
+    assign s3_valid = (connect3 && (state != msb1 && state != msb2)) ? m1_valid : 0;
 
     assign m1_ready = (connect1) ? s1_ready : (connect2) ? s2_ready : (connect3) ? s3_ready: 0;
 
